c3638f354ed9334b486e8fc6823b0f9f5667fad3
SeungCheol <ssc6839@gmail.com>
2021-07-01 21:43:30 +0900

    learn-express

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
     console.log('app.use 미들웨어!!!')
@@ -11,7 +21,12 @@ app.use((req, res, next) => {
 })
 
 app.get('/',  (req, res) => {
+    req.session.id = 'hello'
     res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/:name', (req, res) => {
+    res.send(`hello ${name}`)
 })
 
 app.use((req, res, next) => {
