5f6f4057f247d9f9efd5e4730257e27ae2a0c991
SeungCheol <ssc6839@gmail.com>
2021-07-04 16:25:05 +0900

    learn-sequelize

---
+++ learn-sequelize/models/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+const User = require('./user')
+const Comment = require('./comment')
+
+const env = process.env.NODE_ENV || 'development'
+const config = require('../config/config')[env]
+const db = {}
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
+db.sequelize = sequelize
+
+db.User = User
+db.Comment = Comment
+
+User.init(sequelize)
+Comment.init(sequelize)
+
+module.exports = db
